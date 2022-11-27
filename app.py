1e0ef125988ac6708b61bd89d25ed7518b65d973
Martijn-1052707 <113018953+Martijn-1052707@users.noreply.github.com>
2022-11-27 18:43:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,51 @@
-from flask import Flask, render_template, url_for, session, redirect
-import sqlite3
+from flask import Flask, session, render_template, redirect, url_for, request
+import re
+import sqlite3 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'your secret key'
 
-@app.route('/login')
+# This command creates the "<application directory>/databases/testcorrect_vragen.db" path
+connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
+
+# login page, we use GET and POST requests
+# @app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html')
+    connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
+    connection.row_factory = sqlite3.Row 
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM accounts WHERE username = ? AND password = ?', (username, password,))
+         # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Accessing columns by name instead of by index
+            for account in cursor:
+                session['loggedin'] == True
+                session['id'] == account['id']
+                session['username'] == account['username']
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return redirect(url_for('home')) 
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
 
 # profile page only accessible for loggedin users
 @app.route('/profile')
@@ -31,5 +67,5 @@ def profile():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.debug = True
+    app.run()
