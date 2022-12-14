2b066a7d6f3a9139b8c9b53ee03a3640dbcc40ff
Martijn-1052707 <113018953+Martijn-1052707@users.noreply.github.com>
2022-12-14 23:30:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -56,15 +56,6 @@ def home():
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
-@app.route('/home/auteurs')
-def auteurs():
-    cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM auteurs")
-    columns = [columns[0] for columns in cursor.description]        
-    rows = cursor.fetchall()
-    return render_template('home.html', rows=rows, columns=columns)
-
-
 @app.route('/home/vragen')
 @app.route('/home/vragen/<int:start>/<int:eind>')
 def vragen(start=0, eind=10):
@@ -91,7 +82,64 @@ def leerdoelen():
     rows = cursor.fetchall()
     return render_template('home.html', rows=rows, columns=columns)
 
+@app.route('/')
+@app.route('/filtering/')
+def hello_world():
+    tables = question_model.get_tables()
+    return render_template('list_tables.html', tablenames=tables)
 
+@app.route('/filtering/<table_name>')
+def filter_table(table_name):
+    columns = question_model.get_columns(table_name)
+    return render_template('list_columns.html', columns=columns, table=table_name)
+
+@app.route('/filtering/<table_name>/<column_name>/')
+def filter_table_on_column(table_name, column_name):
+    datatype = "boolean"
+    values = question_model.get_unconvertable_values(table_name, column_name, datatype)
+    return render_template("list_unconvertable.html", values=values, table=table_name, column=column_name, datatype=datatype)
+# add user
+@app.route('/home/add_user', methods=['POST','GET'])
+def add_user():
+    connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO accounts (username, password, email) values (?,?,?)',(username, password, email))
+        connection.commit()
+        flash('User Added','success')
+        return redirect(url_for("add_user"))
+    return render_template("add_user.html")
+
+@app.route("/edit_user/<string:id>",methods=['POST','GET'])
+def edit_user(id):
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
+        cursor = connection.cursor()
+        cursor.execute("update accounts set username=?,password=?,email=? where id=?",(username,password,email,id))
+        connection.commit()
+        flash('User Updated','success')
+        return redirect(url_for("home"))
+    connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute("select * from accounts where id=?",(id,))
+    data = cursor.fetchone()
+    return render_template("edit_user.html",datas=data)
+    
+@app.route("/delete_user/<string:id>",methods=['GET'])
+def delete_user(id):
+    connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
+    cursor = connection.cursor()
+    cursor.execute("delete from accounts where id=?",(id,))
+    connection.commit()
+    flash('User Deleted','warning')
+    return redirect(url_for("home"))
 
 # profile pageonly accessible for loggedin users
 @app.route('/profile')
