e8cfbf328a0a72d3db9e897f805c83235735a262
Martijn-1052707 <1052707@hr.nl>
2022-12-30 16:35:21 +0100

    update main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, session, render_template, redirect, url_for, request, flash
+from flask import Flask, session, render_template, redirect, url_for, request, flash, make_response
 import sqlite3
+import csv
+from io import StringIO
 
 from lib.questionmodel import QuestionModel
 
@@ -69,6 +71,8 @@ def home():
 # get leerdoelen list
 @app.route('/home/leerdoelen')
 def leerdoelen():
+    if 'loggedin'  not in session:
+        return redirect(url_for('login'))
     connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
@@ -79,12 +83,33 @@ def leerdoelen():
 # get auteurs list
 @app.route('/home/auteurs')
 def auteurs():
+    if 'loggedin'  not in session:
+        return redirect(url_for('login'))
     connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM auteurs')
     data = cursor.fetchall()
     return render_template("auteurs.html", auteurs=data)
+
+# zoek de lijst van vragen
+@app.route('/home/vragen')
+def questions():
+    if 'loggedin'  not in session:
+        return redirect(url_for('login'))
+    cursor = connection.cursor()
+    column = cursor.execute('''SELECT * FROM vragen''')
+    rows = cursor.fetchall()
+    invalid_questions_ids = find_invalid_questions()
+    invalid_questions_status = {}
+    for row in rows:
+        if int(row[0]) in invalid_questions_ids:
+            invalid_questions_status[row[0]] = True
+        else:
+            invalid_questions_status[row[0]] = False
+    column = [column[0] for column in column.description]
+    column.append('action')
+    return render_template('vragen.html', rows=rows, columns=column, invalid_questions_status=invalid_questions_status, questions_page=True)
 
 # deze functie return de lijst met ids van ongeldige vragen
 def find_invalid_questions():
@@ -109,6 +134,7 @@ def find_authors():
     rows = cursor.fetchall()    
     return rows
 
+<<<<<<< Updated upstream
 # edit vragen
 @app.route('/home/vragen')
 def vragen():
@@ -123,14 +149,53 @@ def vragen():
 @app.route('/home/vragen/opslaan/<question_id>', methods=['POST'])
 def opslaan(question_id):
     question_content = request.form["vraag"]
+=======
+# edit vraag
+@app.route('/home/edit_question/<int:id>', methods=['GET', 'POST'])
+def edit_question(id):
     cursor = connection.cursor()
-    cursor.execute("UPDATE vragen SET vraag = '" + question_content + "' where id = " + question_id)
-    connection.commit()
-    return redirect(url_for("vragen"))
+    if 'loggedin'  not in session:
+        return redirect(url_for('login'))
+    if request.method == 'GET':
+        cursor.execute('''SELECT * FROM vragen WHERE id =?''', (id,))
+        question = cursor.fetchone()
+        if question:
+            learning_objectives = find_learning_objectives()
+            authors = find_authors()
+            return render_template('edit_question.html', question = question, learning_objectives = learning_objectives, authors=authors)
+        else:
+            return redirect(url_for('home'))
+    elif request.method == 'POST':
+        question = request.form['question']
+        learning_objective = request.form['learning_objective']
+        author = request.form['author']
+        cursor.execute('''UPDATE vragen SET vraag = ?, leerdoel = ?, auteur = ? WHERE id = ?''', (question, learning_objective, author, id))
+        connection.commit()
+        flash('Vraag updated','success')
+        return redirect(url_for('questions'))
+
+# set vraag als exceptie
+@app.route('/home/set_question_as_exception/<int:id>')
+def set_question_as_exception(id):
+>>>>>>> Stashed changes
+    cursor = connection.cursor()
+    if 'loggedin' not in session:
+        return redirect(url_for('login'))
+    cursor.execute('''SELECT * FROM exceptie_vraag where vraag_id = ?''', (id,))
+    exception_question = cursor.fetchone()
+    cursor.execute('''SELECT * FROM vragen WHERE id =?''', (id,))
+    question = cursor.fetchone()
+    if not exception_question and question:
+        cursor.execute('''INSERT INTO exceptie_vraag (vraag_id) values(?)''', (id,))
+        connection.commit()
+        flash('Exceptie updated','success')
+    return redirect(url_for('questions'))
 
 # add user
 @app.route('/home/add_user', methods=['POST','GET'])
 def add_user():
+    if 'loggedin'  not in session:
+        return redirect(url_for('login'))
     connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         username = request.form['username']
@@ -139,7 +204,7 @@ def add_user():
         cursor = connection.cursor()
         cursor.execute('INSERT INTO accounts (username, password, email) values (?,?,?)',(username, password, email))
         connection.commit()
-        flash('User Added','success')
+        flash('Gebruiker Added','success')
         return redirect(url_for("add_user"))
     return render_template("add_user.html")
 
@@ -154,7 +219,7 @@ def edit_user(id):
         cursor = connection.cursor()
         cursor.execute("update accounts set username=?,password=?,email=? where id=?",(username,password,email,id))
         connection.commit()
-        flash('User Updated','success')
+        flash('Gebruiker Updated','success')
         return redirect(url_for("home"))
     connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
     connection.row_factory = sqlite3.Row
@@ -163,6 +228,7 @@ def edit_user(id):
     data = cursor.fetchone()
     return render_template("edit_user.html",datas=data)
 
+# edit auteurs
 @app.route("/edit_auteurs/<string:id>",methods=['POST','GET'])
 def edit_auteurs(id):
     if request.method=='POST':
@@ -229,6 +295,84 @@ def profile():
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
+@app.route('/display')
+def display_rows():
+  connection = sqlite3.connect('./databases/testcorrect_vragen.db', check_same_thread = False)
+  cursor = connection.cursor()
+  # Select rows where column_name is Null
+  cursor.execute("SELECT * FROM vragen WHERE leerdoel IS Null")
+  rows = cursor.fetchall()
+  connection.close()
+  return render_template('display.html', rows=rows)
+
+# get tables
+@app.route('/filtering/')
+def hello_world():
+    tables = question_model.get_tables()
+    return render_template('list_tables.html', tablenames=tables)
+
+# get columns
+@app.route('/filtering/<table_name>')
+def filter_table(table_name):
+    columns = question_model.get_columns(table_name)
+    return render_template('list_columns.html', columns=columns, table=table_name)
+
+# get uncovertable values
+@app.route('/filtering/<table_name>/<column_name>/')
+def filter_table_on_column(table_name, column_name):
+    datatype = "boolean"
+    values = question_model.get_unconvertable_values(table_name, column_name, datatype)
+    return render_template("list_unconvertable.html", values=values, table=table_name, column=column_name, datatype=datatype)
+
+# select-table
+@app.route('/select-table')
+def select_table():
+    # Create a cursor
+    cursor = connection.cursor()
+
+    # Get the list of tables
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+
+    # Fetch the tables
+    tables = cursor.fetchall()
+
+    # Close the cursor
+    cursor.close()
+
+    return render_template('select-table.html', tables=tables)
+
+# display-table
+@app.route('/display-table', methods=['POST'])
+def display_table():
+
+    # Get the selected table
+    table = request.form['table']
+    
+    # Create a cursor
+    cursor = connection.cursor()
+
+    # Select all rows where the column is NULL
+    cursor.execute(f"SELECT * FROM {table} WHERE leerdoel IS NULL")
+    
+    # Fetch the rows
+    rows = cursor.fetchall()
+   
+    # Get the column names
+    cursor.execute(f"PRAGMA table_info( {table} )")
+
+    columns = cursor.fetchall()
+
+    # Get the list of tables
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+
+    # Fetch the tables
+    tables = cursor.fetchall()
+
+    # Close the cursor
+    cursor.close()
+
+    return render_template('select-table.html', tables=tables, table=table, rows=rows, columns=columns)
+
 # logout page
 @app.route('/logout')
 def logout():
@@ -239,24 +383,26 @@ def logout():
    # redirect to login page
    return redirect(url_for('login'))
 
-# @app.route('/')
-@app.route('/filtering/')
-def hello_world():
-    tables = question_model.get_tables()
-    return render_template('list_tables.html', tablenames=tables)
-
-@app.route('/filtering/<table_name>')
-def filter_table(table_name):
-    columns = question_model.get_columns(table_name)
-    return render_template('list_columns.html', columns=columns, table=table_name)
-
-@app.route('/filtering/<table_name>/<column_name>/')
-def filter_table_on_column(table_name, column_name):
-    datatype = "boolean"
-    values = question_model.get_unconvertable_values(table_name, column_name, datatype)
-    return render_template("list_unconvertable.html", values=values, table=table_name, column=column_name, datatype=datatype)
-
-# run app
+# run python application
 if __name__ == '__main__':
     app.run(debug=True)
     app.run()
+
+# # edit vragen
+# @app.route('/home/vragen_update')
+# @app.route('/home/vragen_update/<int:start>/<int:eind>')
+# def vragen():
+#     cursor = connection.cursor()
+#     cursor.execute(f"SELECT * FROM vragen")
+#     columns = [columns[0] for columns in cursor.description]
+#     rows = cursor.fetchall()
+#     return render_template('vragen_update.html', rows=rows, columns=columns)
+
+# # modal vragen
+# @app.route('/home/vragen_update/opslaan/<question_id>', methods=['POST'])
+# def opslaan(question_id):
+#     question_content = request.form["vraag"]
+#     cursor = connection.cursor()
+#     cursor.execute("UPDATE vragen SET vraag = '" + question_content + "' where id = " + question_id)
+#     connection.commit()
+#     return redirect(url_for("vragen_update"))
